7960938d81d6ae637fc47ea5dea4a6da5c930cca
John Imeobong <imeobongjohn38@gmail.com>
2025-05-14 06:47:53 +0100

    Chapter 3

--- Chapter 3/Mathematical_constant.py
+++ Chapter 3/Mathematical_constant.py
@@ -7,6 +7,6 @@ for i in range(terms):
 	counter += (-1.0)**i / (2.0 * i + 1)
 
 
-PI_approx = 4 * counter  
+PI_approx = 4 * counter
 print('Approximated value of PI with', terms, 'terms is:', PI_approx)
 
