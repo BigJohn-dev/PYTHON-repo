584cc54175db977e07ab60130dda0f5bb8b6b545
John Imeobong <imeobongjohn38@gmail.com>
2025-05-27 14:06:42 +0100

    phase gate

---
+++ Phase Gate/To-Do-List Manager/to_do_list_test.py
@@ -0,0 +1,16 @@
+import unittest
+from to_do_list import task_manager, to_do_task
+
+class TestToDoList(unittest.TestCase):
+
+	def test_that_to_do_list_exist(self):
+		self.task = task_manager()
+
+	def test_that_to_do_list_task_can_be_created(self):
+		tasks = to_do_task("add task", "View task", "mark task", "delete task")
+	
+	def test_that_add_task_function_works(self):
+		tasks = to_do_task("add task", "View task", "mark task", "delete task")
+		result = tasks.add_task("Groceries")
+		self.assertEqual(tasks.view_task, "View task")
+		self.assertEqual(result, "New Task added")
