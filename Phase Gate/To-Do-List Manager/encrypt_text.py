d4487ada891e25151a77554abee29eb3c6007980
John Imeobong <imeobongjohn38@gmail.com>
2025-05-27 13:52:57 +0100

    encrypt_text

---
+++ Phase Gate/To-Do-List Manager/encrypt_text.py
@@ -0,0 +1,13 @@
+# Encrypt-text
+
+alphabets = {a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z}
+
+def encrypt_text(sentence):
+	words = []
+	words.append(sentence)
+	
+	for text in range(len(sentence)):
+		text.append("13")
+		return text
+
+print(encrypt_text("Hello World"))
--- Phase Gate/To-Do-List Manager/to_do_list_test.py
+++ Phase Gate/To-Do-List Manager/to_do_list_test.py
@@ -8,9 +8,14 @@ class TestToDoList(unittest.TestCase):
 
 	def test_that_to_do_list_task_can_be_created(self):
 		tasks = to_do_task("add task", "View task", "mark task", "delete task")
-		
+	
 	def test_that_add_task_function_works(self):
 		tasks = to_do_task("add task", "View task", "mark task", "delete task")
 		result = tasks.add_task("Groceries")
 		self.assertEqual(tasks.view_task, "View task")
 		self.assertEqual(result, "New Task added")
+
+	def test_that_to_view_tasks_function_works(self):
+		tasks = to_do_task("add task", "View task", "mark task", "delete task")
+		result = tasks.view_task()
+		self.assertEqual(result, "This are your to-do task Groceries")
