c07e7b4548b1722aa5b364e39bba414791c6deb3
John Imeobong <imeobongjohn38@gmail.com>
2025-07-06 22:15:22 +0100

    Add files via upload

--- CheckPoint/Day 2/nested_loop_triangle.py
+++ CheckPoint/Day 2/nested_loop_triangle.py
@@ -1,5 +1,18 @@
 # printing triangle below the other using nested loop
 
-for i in range(1, 10):
-    print("*", end = " ")
+for i in range(1, 11):
+    for j in range(1, i + 1):
+        print("*", end = " ")
+    print()
+
+print()
+for i in range(10, 0, -1):
+    for j in range(1, i + 1):
+        print("*", end = " ")
+    print()
+
+print()
+for i in range(10, 0, -1):
+    for j in range(1, i + 1):
+        print(" *", end = " ")
     print()
