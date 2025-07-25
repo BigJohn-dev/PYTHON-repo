f7189e82d36258dd7cc98d5b3605c5f7dd7091e8
John Imeobong <imeobongjohn38@gmail.com>
2025-07-25 15:05:46 +0100

    Add files via upload

--- Python on PYCharm/regex_examples.py
+++ Python on PYCharm/regex_examples.py
@@ -1,10 +1,23 @@
 import re
 
-sentence = "The silent boy is sic today, call this number 1234567890"
+ # Matches a U.S. phone number in the format 123-456-7890
+phone = "123-456-7890"
+# phone = "my account number is 0092455368"
+pattern = re.findall(r'^\d{3}-\d{3}-\d{4}$',phone)
+print(pattern)
 
-# pattern = re.findall(r'\b[A-Za-z]+\b', sentence)
-# pattern = re.compile('[A-Za-z]+')
+# An email: abc@gmail.com
+gmail = "Yemi@yemi@gmail.com"
+# gmail = "tolu@gmail.com"
+patternII = re.findall(r'([a-z@0-9a-z.a-z]+)', gmail)
+print(patternII)
 
-pattern = re.compile(r'[^0-9]+')
-match = pattern.findall(sentence)
-print(match)
+# Count and return words that starts with uppercase
+text = "Alice and Bob are Good Friends."
+pattern_III = re.findall(r'[A-Z]\w+', text)
+print(pattern_III)
+
+# Split sentence into words ignoring punctuations
+sentence = "Hello! How are you doing?"
+patternIV = re.findall(r'[A-Za-z]\w+', sentence)
+print(patternIV)
